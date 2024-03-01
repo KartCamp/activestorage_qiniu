388ba4d2fe9de51eed2b0013a0d1c7b94addb6bc
LU <maklu.cn@gmail.com>
2024-03-01 21:52:37 +0900

    Update qiniu_service.rb

--- lib/active_storage/service/qiniu_service.rb
+++ lib/active_storage/service/qiniu_service.rb
@@ -139,7 +139,7 @@ module ActiveStorage
       end
     end
 
-    def url_for_direct_upload(key, expires_in:, content_type:, content_length:, checksum:)
+    def url_for_direct_upload(key, expires_in:, content_type:, content_length:, checksum:, **others)
       instrument :url, key: key do |payload|
         url = Qiniu::Config.up_host(bucket)
         payload[:url] = url
